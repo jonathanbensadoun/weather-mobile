cd13fec028bb3d8c256c17775f8950f6a2ab0c8b
jonathanBensadoun <jonathan.ben-sadoun@oclock.school>
2024-09-28 18:57:08 +0200

    change color of bg splash

--- app/index.tsx
+++ app/index.tsx
@@ -90,7 +90,7 @@ export default function WeatherApp() {
         `https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${latitude}&lon=${longitude}`
       );
       const data = await response.json();
-      console.log("data", data);
+      // console.log("data", data);
       setCityName(data.address);
     } catch (error) {
       console.error(
